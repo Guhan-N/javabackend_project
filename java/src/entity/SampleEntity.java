dec141a7bb4afefc503d40de4b272142a6749890
Guhan-224 <guhan6575@gmail.com>
2025-07-30 11:48:26 +0530

    done

--- java/src/entity/SampleEntity.java
+++ java/src/entity/SampleEntity.java
@@ -4,7 +4,8 @@ public class SampleEntity {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("WeLcOmE.,.,.,.")
+		System.out.println("WeLcOmE.,.,.,.");
+		System.out.print("WoRlD.,.,.,.,.,.");
 
 	}
 
